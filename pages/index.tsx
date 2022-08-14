a272700aefd9f8447a219d740ff1ebaedeacb0c1
GeneralDido <dimpanouris@gmail.com>
2022-08-14 10:58:42 +0200

    add HeroHeading, Section, classNames, i18next, colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
-import type { NextPage } from 'next'
+import React from 'react'
+import type {GetStaticProps, NextPage} from 'next'
+import HeroHeading from '../components/HeroHeading/HeroHeading'
+import { useTranslation } from 'next-i18next'
+import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
+import Section from '../components/Section/Section'
+
 
 const Home: NextPage = () => {
+  const { t } = useTranslation('common')
   return (
-    <div/>
+    <Section>
+      <HeroHeading
+        title={t('heroHeading.title')}
+        subtitle={t('heroHeading.subtitle')}
+        className={'mt-20 mb-4'}
+      />
+    </Section>
   )
 }
 
 export default Home
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale!, ['common'])),
+  },
+})
