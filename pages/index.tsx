c2133cfd1956e5a6680dfe7428b9b29528c3c12a
GeneralDido <dimpanouris@gmail.com>
2022-08-17 01:25:28 +0200

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import type {GetStaticProps, NextPage} from 'next'
-import { useTranslation } from 'next-i18next'
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
 import AllSections from '../components/Sections/AllSections/AllSections'
 
-
 const Home: NextPage = () => {
-  const { t } = useTranslation('common')
-  return (
-    <AllSections/>
-  )
+  return <AllSections/>
 }
 
 export default Home
