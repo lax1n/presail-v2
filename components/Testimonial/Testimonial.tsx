9f8e5c36f3257dae884697df5780dab5c8618daa
GeneralDido <dimpanouris@gmail.com>
2022-08-16 23:09:11 +0200

    make Testimonial component and refactor

---
+++ components/Testimonial/Testimonial.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import styles from './Testimonial.module.css'
+import Image from 'next/image'
+
+interface TestimonialProps {
+  header?: string
+  description: string
+  name?: string
+  logoPath?: string
+}
+
+const Testimonial: React.FC<TestimonialProps> = (props) => {
+  const { header, description, name, logoPath } = props
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.quoteLeft}>
+        <Image
+          alt={''}
+          src='/images/QuoteLeft.svg'
+          width={'20px'}
+          height={'20px'}
+        />
+      </div>
+      <div className={'ml-8'}>
+        <div className={styles.header}>
+          {header}
+        </div>
+
+        <div className={'inline-flex'}>
+          <div className={styles.testimonial}>
+            {description}
+          </div>
+          <div className={styles.quoteRight}>
+            <Image
+              alt={''}
+              src='/images/QuoteRight.svg'
+              width={'20px'}
+              height={'20px'}
+            />
+          </div>
+        </div>
+        <div className={styles.line}/>
+        <div className={styles.items}>
+          <div className={styles.name}>
+            {name}
+          </div>
+          {logoPath &&
+          <div className={styles.logo}>
+            <Image
+              alt={''}
+              src={logoPath}
+              width={'81px'}
+              height={'16px'}
+            />
+          </div>
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Testimonial
