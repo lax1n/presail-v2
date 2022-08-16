35630855e98462cf383c8a5f46e31723dd10ec27
GeneralDido <dimpanouris@gmail.com>
2022-08-17 00:56:48 +0200

    add more images

--- components/Sections/Section4/Section4.tsx
+++ components/Sections/Section4/Section4.tsx
@@ -2,11 +2,21 @@ import React from 'react'
 import Testimonial from '../../Testimonial/Testimonial'
 import Section from '../../Section/Section'
 import {useTranslation} from 'next-i18next'
+import Image from 'next/image'
+import styles from './Section4.module.css'
 
 const Section4 = () => {
   const { t } = useTranslation('common')
   return (
     <Section>
+      <div className={styles.investorPortal}>
+        <Image
+          alt={'Presail Investor Portal'}
+          src='/images/05InvestorPortal.webp'
+          layout={'fill'}
+          objectFit={'contain'}
+        />
+      </div>
       <Testimonial
         header={t('testimonial.header')}
         description={t('testimonial.description')}
