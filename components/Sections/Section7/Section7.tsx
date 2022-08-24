e6e64a459e4614a9637726cbda15d5181d4a523f
GeneralDido <dimpanouris@gmail.com>
2022-08-24 19:32:33 +0200

    add alloSwap

--- components/Sections/Section7/Section7.tsx
+++ components/Sections/Section7/Section7.tsx
@@ -3,11 +3,20 @@ import Section from '@/components/Section/Section'
 import SectionHeading from '@/components/SectionHeading/SectionHeading'
 import {useTranslation} from 'next-i18next'
 import styles from './Section7.module.css'
+import Image from 'next/image'
 
 const Section7 = () => {
   const { t } = useTranslation('common')
   return (
     <Section  colorScheme={'dark'}>
+      <div className={styles.alloSwap}>
+        <Image data-aos={'fade-left'}
+          alt={'Presail AlloSwap OTC'}
+          src={'/images/Section7/AlloSwapOTC.svg'}
+          width={'377px'}
+          height={'392px'}
+        />
+      </div>
       <div className={styles.sectionHeading}>
         <SectionHeading
           header={t('bestOption.header')}
