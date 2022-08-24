3a2f9d62971287967bc813e6e1d1c8664a460030
GeneralDido <dimpanouris@gmail.com>
2022-08-24 22:01:14 +0200

    use Cal and calconfig

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './Button.module.css'
 import classNames from 'classnames'
 import '@calcom/embed-react'
+import Cal from '@calcom/embed-react'
 
 interface ButtonProps {
   text: string
@@ -12,9 +13,12 @@ const Button: React.FC<ButtonProps> = (props) => {
   const { text, className } = props
 
   return (
-    <button data-cal-link="even-presail/30min" className={classNames(styles.btn, className)}>
-      {text}
-    </button>
+    <div>
+      <Cal calLink={'even-presail/30min'} className={'hidden'}/>
+      <button data-cal-link={'even-presail/30min'} data-cal-config={'/public/calconfig.json'} className={classNames(styles.btn, className)}>
+        {text}
+      </button>
+    </div>
   )
 }
 
