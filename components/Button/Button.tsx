58dffabf9fe4f8c8b069d2648306450f00df7715
GeneralDido <dimpanouris@gmail.com>
2022-08-24 20:10:58 +0200

    use calcom for schedule calls

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Button.module.css'
 import classNames from 'classnames'
+import '@calcom/embed-react'
 
 interface ButtonProps {
   text: string
@@ -11,9 +12,11 @@ const Button: React.FC<ButtonProps> = (props) => {
   const { text, className } = props
 
   return (
-    <button  className={classNames(styles.btn, className)}>
-      {text}
-    </button>
+    <div>
+      <button data-cal-link='even-presail/30min' className={classNames(styles.btn, className)}>
+        {text}
+      </button>
+    </div>
   )
 }
 
