0f2ee110dab9d25c603f974ecb182f83dcf8a54b
GeneralDido <dimpanouris@gmail.com>
2022-08-25 12:39:09 +0200

    remove unused prop from Section component

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -4,29 +4,22 @@ import styles from './Section.module.css'
 
 export type colorScheme = 'light' | 'dark'
 
-export type style = 'static' | 'dynamic'
-
 interface SectionProps {
   className?: string
   colorScheme?: colorScheme
   children?: React.ReactNode
-  style?: style
 }
 
 const Section: React.FC<SectionProps> = (props) => {
-  const { children, className, colorScheme='light', style='dynamic' } = props
+  const { children, className, colorScheme='light' } = props
 
   const sectionStyle = classNames(styles.section, {
     [styles.light]: colorScheme === 'light',
     [styles.dark]: colorScheme === 'dark',
   })
 
-  const section = classNames(sectionStyle, {
-    [styles.static]: style === 'static',
-  })
-
   return (
-    <div className={classNames(className, section)}>
+    <div className={classNames(className, sectionStyle)}>
       {children}
     </div>
   )
